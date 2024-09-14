d1cdcfa52e4e9288e4483a480faee46c9ffc5493
Björn Þór Jónsson <bangsi@bthj.is>
2024-09-14 11:23:52 +0200

    model manager for dimensionality reduction

--- measurements/diversity/dimensionality_reduction.py
+++ measurements/diversity/dimensionality_reduction.py
@@ -1,80 +1,57 @@
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import MinMaxScaler
-import pickle as pkl
-import time
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 from umap.parametric_umap import ParametricUMAP, load_ParametricUMAP
 import os
+import pickle as pkl
 from scipy.stats import entropy
 
-def plot_expected_variance_ratio(pca):
-    print('Printing explained variance ratio...')
+class ModelManager:
+    def __init__(self, evorun_dir):
+        self.evorun_dir = evorun_dir
+        self.pca = None
+        self.scaler = None
+        self.umap = None
+        self.max_reconstruction_error = None
+        self.max_complexity = None
+        self.max_smoothness = None
 
-    exp_var_pca = pca.explained_variance_ratio_
-    cum_sum_eigenvalues = exp_var_pca.cumsum()
+    def save_model(self):
+        os.makedirs(self.evorun_dir, exist_ok=True)
+        if self.pca is not None:
+            pkl.dump(self.pca, open(os.path.join(self.evorun_dir, 'pca_model.pkl'), 'wb'))
+        if self.scaler is not None:
+            pkl.dump(self.scaler, open(os.evorun_dir + 'scaler.pkl', 'wb'))
+        if self.umap is not None:
+            self.umap.save(os.path.join(self.evorun_dir, 'umap_model'))
+        if self.max_reconstruction_error is not None:
+            np.save(os.path.join(self.evorun_dir, 'max_reconstruction_error.npy'), self.max_reconstruction_error)
+        if self.max_complexity is not None:
+            np.save(os.path.join(self.evorun_dir, 'max_complexity.npy'), self.max_complexity)
+        if self.max_smoothness is not None:
+            np.save(os.path.join(self.evorun_dir, 'max_smoothness.npy'), self.max_smoothness)
 
-    plt.bar(range(0,len(exp_var_pca)), exp_var_pca, alpha=0.5, align='center', label='Individual explained variance')
-    plt.step(range(0,len(cum_sum_eigenvalues)), cum_sum_eigenvalues, where='mid',label='Cumulative explained variance')
-    plt.ylabel('Explained variance ratio')
-    plt.xlabel('Principal component index')
-    plt.legend(loc='best')
-    plt.tight_layout()
-    # save the plot to disk
-    plt.savefig(f'/tmp/{time.time()}_pca_explained_variance_ratio.png')
+    def load_model(self):
+        if os.path.exists(os.path.join(self.evorun_dir, 'pca_model.pkl')):
+            self.pca = pkl.load(open(os.path.join(self.evorun_dir, 'pca_model.pkl'), 'rb'))
+        if os.path.exists(os.path.join(self.evorun_dir, 'scaler.pkl')):
+            self.scaler = pkl.load(open(os.path.join(self.evorun_dir, 'scaler.pkl'), 'rb'))
+        if os.path.exists(os.path.join(self.evorun_dir, 'umap_model')):
+            self.umap = load_ParametricUMAP(os.path.join(self.evorun_dir, 'umap_model'))
+        if os.path.exists(os.path.join(self.evorun_dir, 'max_reconstruction_error.npy')):
+            self.max_reconstruction_error = np.load(os.path.join(self.evorun_dir, 'max_reconstruction_error.npy'))
+        if os.path.exists(os.path.join(self.evorun_dir, 'max_complexity.npy')):
+            self.max_complexity = np.load(os.path.join(self.evorun_dir, 'max_complexity.npy'))
+        if os.path.exists(os.path.join(self.evorun_dir, 'max_smoothness.npy')):
+            self.max_smoothness = np.load(os.path.join(self.evorun_dir, 'max_smoothness.npy'))
 
-    plt.clf()
+model_managers = {}
 
-pca = None
-scaler = None
-
-# TODO: should reload model
-
-def get_pca_projection(features, n_components=2, should_fit=True, evorun_dir='', plot_variance_ratio=False, components_list=[]):
-    global pca
-    global scaler
-    if should_fit:
-        print('Fitting PCA model...')
-        # if components_list has been set, use the default PCA constructor, which will use all components, and then use the components_list to select the components
-        if len(components_list) > 0:
-            pca = PCA()
-        else: # otherwise use n_components
-            pca = PCA(n_components=n_components)
-        pca.fit(features)
-        pkl.dump(pca, open(evorun_dir + 'pca_model.pkl', 'wb'))
-    else:
-        # check if pca variable is set
-        if pca is None:
-            print('Loading PCA model...')
-            # assume that the PCA model has been saved to disk - TODO: throw an error if it hasn't?
-            pca = pkl.load(open(evorun_dir + 'pca_model.pkl', 'rb'))
-
-    if plot_variance_ratio:
-        plot_expected_variance_ratio(pca)
-
-    transformed = pca.transform(features)
-
-    if should_fit:
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaler.fit(transformed)
-        pkl.dump(scaler, open(evorun_dir + 'scaler.pkl', 'wb'))
-    else:
-        # check if scaler variable is set
-        if scaler is None:
-            print('Loading scaler...')
-            # assume that the scaler has been saved to disk - TODO: throw an error if it hasn't?
-            scaler = pkl.load(open(evorun_dir + 'scaler.pkl', 'rb'))
-
-    scaled = scaler.transform(transformed)
-
-    # pick the components indexto to in components_list
-    if len(components_list) > 0:
-        scaled = scaled[:, components_list]
-
-    return scaled
-
-
+def get_model_manager(evorun_dir):
+    if evorun_dir not in model_managers:
+        model_managers[evorun_dir] = ModelManager(evorun_dir)
+    return model_managers[evorun_dir]
 
 def create_autoencoder(input_dim, latent_dim, random_state):
     tf.random.set_seed(random_state)
@@ -92,100 +69,122 @@ def create_autoencoder(input_dim, latent_dim, random_state):
     
     return encoder, decoder
 
-def calculate_reconstruction_loss(umap_model, features):
-    reconstructed = umap_model.decoder(umap_model.transform(features)).numpy()
+def calculate_reconstruction_loss(model, features):
+    if isinstance(model, ParametricUMAP):
+        reconstructed = model.decoder(model.transform(features)).numpy()
+    else:  # PCA
+        reconstructed = model.inverse_transform(model.transform(features))
     return np.mean(np.square(features - reconstructed), axis=1)
 
 def calculate_complexity(feature_vector):
-    # Calculate entropy as a measure of complexity
-    # hist, _ = np.histogram(features, bins=20)
-    # return entropy(hist + 1e-10)  # Add small constant to avoid log(0)
     hist, _ = np.histogram(feature_vector, bins='auto', density=True)
-    return entropy(hist + 1e-10)  # Add small constant to avoid log(0)
+    return entropy(hist + 1e-10)
 
 def calculate_smoothness(feature_vector):
-    # Calculate the inverse of the average gradient as a measure of smoothness
-    # gradients = np.diff(features, axis=1)
-    # return 1 / (np.mean(np.abs(gradients)) + 1e-10)  # Add small constant to avoid division by zero
     total_variation = np.sum(np.abs(np.diff(feature_vector)))
-    return 1 / (total_variation + 1e-10)  # Add small constant to avoid division by zero
+    return 1 / (total_variation + 1e-10)
 
 def calculate_novelty_score(reconstruction_loss, max_reconstruction_error, feature_vector, max_complexity, max_smoothness, alpha=10, beta=0.5, gamma=0.5):
     normalized_loss = reconstruction_loss / max_reconstruction_error
     complexity = calculate_complexity(feature_vector)
     smoothness = calculate_smoothness(feature_vector)
     
-    # Normalize complexity and smoothness
     normalized_complexity = complexity / max_complexity
     normalized_smoothness = smoothness / max_smoothness
     
-    # Combine reconstruction loss, complexity penalty, and smoothness reward
     adjusted_loss = normalized_loss - beta * normalized_complexity + gamma * normalized_smoothness
     
-    # Apply sigmoid to get final novelty score
     return 1 / (1 + np.exp(-alpha * (adjusted_loss - 0.5)))
 
-def get_umap_projection(features, n_components=2, should_fit=True, evorun_dir='', random_state=42, n_neighbors=15, min_dist=0.1):
-    input_dim = features.shape[1]
+def get_pca_projection(features, n_components=2, should_fit=True, evorun_dir='', calculate_novelty=False, components_list=[]):
+    model_manager = get_model_manager(evorun_dir)
     
     if should_fit:
-        encoder, decoder = create_autoencoder(input_dim, n_components, random_state)
+        print('Fitting PCA model...')
+        if len(components_list) > 0:
+            model_manager.pca = PCA()
+        else:
+            model_manager.pca = PCA(n_components=n_components)
+        model_manager.pca.fit(features)
         
-        umap = ParametricUMAP(
+        model_manager.scaler = MinMaxScaler(feature_range=(0, 1))
+        transformed = model_manager.pca.transform(features)
+        model_manager.scaler.fit(transformed)
+        
+        if calculate_novelty:
+            all_reconstruction_losses = calculate_reconstruction_loss(model_manager.pca, features)
+            model_manager.max_reconstruction_error = np.max(all_reconstruction_losses)
+            model_manager.max_complexity = np.max([calculate_complexity(f) for f in features])
+            model_manager.max_smoothness = np.max([calculate_smoothness(f) for f in features])
+        
+        model_manager.save_model()
+    else:
+        model_manager.load_model()
+
+    transformed = model_manager.pca.transform(features)
+    scaled = model_manager.scaler.transform(transformed)
+
+    if len(components_list) > 0:
+        scaled = scaled[:, components_list]
+
+    if calculate_novelty:
+        reconstruction_losses = calculate_reconstruction_loss(model_manager.pca, features)
+        novelty_scores = np.array([
+            calculate_novelty_score(loss, model_manager.max_reconstruction_error, feature, model_manager.max_complexity, model_manager.max_smoothness)
+            for loss, feature in zip(reconstruction_losses, features)
+        ])
+        return scaled, novelty_scores
+    else:
+        return scaled
+
+def get_umap_projection(features, n_components=2, should_fit=True, evorun_dir='', calculate_novelty=False, random_state=42, n_neighbors=15, min_dist=0.1):
+    model_manager = get_model_manager(evorun_dir)
+    
+    if should_fit:
+        input_dim = features.shape[1]
+        if calculate_novelty:
+            encoder, decoder = create_autoencoder(input_dim, n_components, random_state)
+        else:
+            encoder, decoder = None, None
+        
+        model_manager.umap = ParametricUMAP(
             n_components=n_components,
             encoder=encoder,
             decoder=decoder,
-            autoencoder_loss=True,
+            autoencoder_loss=calculate_novelty,
             loss_report_frequency=1,
-            n_epochs=100,  # Adjust as needed
-            batch_size=64,  # Adjust as needed
+            n_epochs=100,
+            batch_size=64,
             random_state=random_state,
             n_neighbors=n_neighbors,
             min_dist=min_dist,
-            init='random'  # Use random initialization for better reproducibility
+            init='random'
         )
         
         print('Fitting UMAP model...')
-        umap.fit(features)
-
-        umap_model_path = os.path.join(evorun_dir, 'umap_model')
-        print('Saving UMAP model to disk...', umap_model_path)
-        if not os.path.exists(umap_model_path):
-            print('Creating directory for UMAP model...', os.path.dirname(umap_model_path))
-            os.makedirs(umap_model_path)
-        umap.save(umap_model_path)
+        model_manager.umap.fit(features)
         
-        # Calculate and save the max values for normalization
-        all_reconstruction_losses = calculate_reconstruction_loss(umap, features)
-        max_reconstruction_error = np.max(all_reconstruction_losses)
-        max_complexity = np.max([calculate_complexity(f) for f in features])
-        max_smoothness = np.max([calculate_smoothness(f) for f in features])
+        if calculate_novelty:
+            all_reconstruction_losses = calculate_reconstruction_loss(model_manager.umap, features)
+            model_manager.max_reconstruction_error = np.max(all_reconstruction_losses)
+            model_manager.max_complexity = np.max([calculate_complexity(f) for f in features])
+            model_manager.max_smoothness = np.max([calculate_smoothness(f) for f in features])
         
-        np.save(os.path.join(evorun_dir, 'max_reconstruction_error.npy'), max_reconstruction_error)
-        np.save(os.path.join(evorun_dir, 'max_complexity.npy'), max_complexity)
-        np.save(os.path.join(evorun_dir, 'max_smoothness.npy'), max_smoothness)
+        model_manager.save_model()
     else:
-        umap_model_path = os.path.join(evorun_dir, 'umap_model')
-        if not os.path.exists(umap_model_path):
-            raise FileNotFoundError(f"UMAP model not found at {umap_model_path}")
-        umap = load_ParametricUMAP(umap_model_path)
-        max_reconstruction_error = np.load(os.path.join(evorun_dir, 'max_reconstruction_error.npy'))
-        max_complexity = np.load(os.path.join(evorun_dir, 'max_complexity.npy'))
-        max_smoothness = np.load(os.path.join(evorun_dir, 'max_smoothness.npy'))
+        model_manager.load_model()
 
-    # Ensure features is 2D
-    if features.ndim == 1:
-        features = features.reshape(1, -1)
+    transformed = model_manager.umap.transform(features)
 
-    transformed = umap.transform(features)
-    reconstruction_losses = calculate_reconstruction_loss(umap, features)
-    
-    novelty_scores = np.array([
-        calculate_novelty_score(loss, max_reconstruction_error, feature, max_complexity, max_smoothness)
-        for loss, feature in zip(reconstruction_losses, features)
-    ])
-
-    return transformed, novelty_scores
+    if calculate_novelty:
+        reconstruction_losses = calculate_reconstruction_loss(model_manager.umap, features)
+        novelty_scores = np.array([
+            calculate_novelty_score(loss, model_manager.max_reconstruction_error, feature, model_manager.max_complexity, model_manager.max_smoothness)
+            for loss, feature in zip(reconstruction_losses, features)
+        ])
+        return transformed, novelty_scores
+    else:
+        return transformed
 
 def set_global_random_state(seed):
     np.random.seed(seed)
